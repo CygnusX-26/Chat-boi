174b4871ff060ff0d0d3ffe728ea88eea2b2866c
CygnusX-26 <42686923+CygnusX-26@users.noreply.github.com>
2022-08-31 22:33:48 -0400

    davinci

--- Bot.py
+++ Bot.py
@@ -67,7 +67,7 @@ async def on_message(message):
         try:
             if getChannel(message.guild.id)[1] == message.channel.name:
                 response = openai.Completion.create(
-                model="text-curie-001",
+                model="text-davinci-002",
                 prompt=f"Marv is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\nMarv: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\nMarv: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\nMarv: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\nMarv: I’m not sure. I’ll ask my friend Google.\nYou: {message.content}\nMarv:",
                 temperature=0.5,
                 max_tokens=60,
